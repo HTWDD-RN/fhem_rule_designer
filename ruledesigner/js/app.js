42749c8b26c4f837597f14a689971baea7580894
Toni Kanther <s71395@htw-dresden.de>
2015-01-17 11:47:51 +0100

    Move config in own file Create Interactions folder Move windows resize interaction Refactoring window_resize.js

--- ruledesigner/js/app.js
+++ ruledesigner/js/app.js
@@ -1,78 +1,9 @@
-/**
- * 
- */
-
-var VIRTUAL_DEVICES = [ {
-	"TYPE" : "VD_DIGITAL_TIMER",
-	"NAME" : "Digital timer",
-	"ALT":	"Digitale Zeitschaltuhr, welche der realen Bauweise nachempfunden ist",
-	"ICON_CLASS" : "RD_DIGITAL_TIMER",
-	"SETS": [
-		"Weekdays",
-		"BeginTime",
-		"EndTime",
-		"BeginDate",
-		"EndDate"
-	],
-	"IS_ACTOR": true,
-	"IS_SENSOR": true,
-	"IS_VDEV" : true
-}, {
-	"TYPE" : "VD_DELAY_TIMER",
-	"NAME" : "Delay timer",
-	"ALT" : "Ermöglicht verzögerte Ausführung von Befehlen und Schaltvorgängen",
-	"ICON_CLASS" : "RD_DELAY_TIMER",
-	"SETS" : [
-	    "","","",
-	],
-	"IS_ACTOR": true,
-	"IS_SENSOR": false,
-	"IS_VDEV" : true
-}, {
-	"TYPE" : "VD_RANDOM_TIMER",
-	"NAME" : "Random timer",
-	"ALT": "Ermöglicht zufallsabhängiges Schalten/ Setzen von Aktoren ",
-	"ICON_CLASS" : "RD_RANDOM_TIMER",
-	"SETS" : [
-	    "UpperBoundTime","LowerBoundTime","SwitchedTime",
-	],
-	"IS_ACTOR": true,
-	"IS_SENSOR": false,
-	"IS_VDEV" : true
-} ]
-
-var Configuration = {
-	DEBUG_LEVEL : 5,
-	// * // failure/ critical,
-	// 1 // none,
-	// 2 // errors,
-	// 3 // warnings,
-	// 4 // info (Inputs/Outputs),
-	// 5 // debug
-
-	GUI : {
-		APP_CONTAINER : '.appContainer',
-		MBOX_CONTAINER : '.alertContainer',
-		TOOLBAR : {
-			CLASSES : '', // 'ui-widget-header ui-widget ui-widget-content
-		// ui-corner-all',
-		},
-		OBJECT_LIST : {
-			CLASSES : 'ui-tabs ui-widget ui-widget-content ui-corner-all',
-			SEGMENTATION : {
-				CLASSES : 'ui-objlist ui-corner-all',
-				HEAD : 'ui-objlist-head ui-widget-header ui-corner-all',
-				BODY : 'ui-objlist-body'
-			}
-		}
-	},
-	HA_SVR_URL : "http://localhost:8083/fhem", // here: set as absolute path
-	WRAPPER : "FHEM",
-	WRAPPER_URL : "wrapper", // here: set as relative path
-	WRAPPER_DEFAULT_SEGMENTATION : "Classic"
-}
-
 var app = function() {
+	
+	// TODO: Load config
+	
+	// TODO: Load window resize
+	
 	$(window).load(function() {
 
 		// Initialize MessageBox
